6fe5bb3b98268c1dc66c55411158e6dd7a52b484
Philisiwe07 <123089991+Philisiwe07@users.noreply.github.com>
2025-09-13 20:21:00 +0200

    created html and javascript for my project

---
+++ index.js
@@ -0,0 +1,27 @@
+document.getElementById("appointmentForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+    let patient = document.getElementById("patient").value;
+    let doctor = document.getElementById("doctor").value;
+    let date = document.getElementById("date").value;
+    let time = document.getElementById("time").value;   
+    fetch("send_sms.php",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"},
+        body: JSON.stringify({
+            patient, doctor, date, time,
+             doctorPhone
+        })
+    }).then(response => response.json())
+    .then(data => {
+        if(data.success) {  
+            alert("Appointment booked and SMS sent!");}
+            else {
+                alert("Appointment booked but failed to send SMS.");
+            }       
+
+    });
+});
+            
+
+
